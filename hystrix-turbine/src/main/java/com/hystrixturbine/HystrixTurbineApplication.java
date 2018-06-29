6ab4e8431cf7b7759916a4b7327ff99f18303f23
jarvisqi <qdw8911@sina.com>
2018-06-29 15:47:00 +0800

    RabbitMQ 基于消息聚合hystrix监控数据

--- hystrix-turbine/src/main/java/com/hystrixturbine/HystrixTurbineApplication.java
+++ hystrix-turbine/src/main/java/com/hystrixturbine/HystrixTurbineApplication.java
@@ -2,7 +2,6 @@ package com.hystrixturbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
