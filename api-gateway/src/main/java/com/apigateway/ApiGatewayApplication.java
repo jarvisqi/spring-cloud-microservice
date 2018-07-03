f795a64bdba3848fc50ae25e6e0e2a890b1c9447
jarvisqi <qdw8911@sina.com>
2018-07-03 16:05:45 +0800

    添加api网关服务

---
+++ api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * spring cloud 服务网关 api-gateway
+ *
+ * @author Jarvis
+ * @date 2018/07/03
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
