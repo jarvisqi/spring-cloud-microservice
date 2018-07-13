dbeac97cb320daa95eeea2dd282f57ba436bcc71
jarvisqi <qdw8911@sina.com>
2018-07-13 13:58:54 +0800

    ZuulFilter 自定义过滤器

--- zuul-gateway/src/main/java/com/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/zuulgateway/ZuulGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.zuulgateway;
 
+import com.zuulgateway.filter.GatewayFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * spring cloud zuul 网关
@@ -16,5 +18,10 @@ public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+    @Bean
+    public GatewayFilter gatewayFilterBean() {
+        return new GatewayFilter();
     }
 }
