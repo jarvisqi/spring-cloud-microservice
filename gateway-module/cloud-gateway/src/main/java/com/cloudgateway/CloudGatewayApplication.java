66225585b68f011f6412e0ad41e741138f295335
jarvisqi <qdw8911@sina.com>
2018-07-13 16:22:25 +0800

    update 网关模块

---
+++ gateway-module/cloud-gateway/src/main/java/com/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring cloud 服务网关 api-gateway
+ *
+ * @author Jarvis
+ * @date 2018/07/03
+ */
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+}
