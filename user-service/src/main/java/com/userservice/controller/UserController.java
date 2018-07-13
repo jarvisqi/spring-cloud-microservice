da0994c3f04b8aaae7f9931fe4cd4403d30a5d47
jarvisqi <qdw8911@sina.com>
2018-07-13 17:17:42 +0800

    eureka client user-service

---
+++ user-service/src/main/java/com/userservice/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.userservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jarvis
+ * @date 2018/6/25
+ */
+@RestController
+public class UserController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
+    public String getUser() {
+        return "Success, User-Service, Port :" + port;
+    }
+
+}
