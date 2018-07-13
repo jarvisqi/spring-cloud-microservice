e547e36287ea98de7adb059bd4361941ad6d3968
jarvisqi <qdw8911@sina.com>
2018-07-13 17:16:35 +0800

    Hystrix Dashboard 监控面板

---
+++ hystrix-dashboard/src/main/java/com/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,22 @@
+package com.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Hystrix Dashboard 断路器监控面板
+ *
+ * @author Jarvis
+ * @date 2018/06/26
+ */
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
