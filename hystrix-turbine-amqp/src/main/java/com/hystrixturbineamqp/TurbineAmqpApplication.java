2195ee8836e61e13bf6d442406656613356fafaa
jarvisqi <qdw8911@sina.com>
2018-07-13 17:13:51 +0800

    使用 RabbitMQ 基于消息代理的Turbine聚合服务

---
+++ hystrix-turbine-amqp/src/main/java/com/hystrixturbineamqp/TurbineAmqpApplication.java
@@ -0,0 +1,20 @@
+package com.hystrixturbineamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+/**
+ * spring cloud 使用 RabbitMQ 基于消息代理的Turbine聚合服务
+ *
+ * @author Jarvis
+ * @date 2018/06/28
+ */
+@EnableTurbineStream
+@SpringBootApplication
+public class TurbineAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineAmqpApplication.class, args);
+    }
+}
