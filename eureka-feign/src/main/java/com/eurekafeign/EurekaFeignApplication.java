a89bf37e486693f8333b8c4953a2ebbdf1cca695
jarvisqi <qdw8911@sina.com>
2018-07-19 13:23:56 +0800

    update EurekaClient

--- eureka-feign/src/main/java/com/eurekafeign/EurekaFeignApplication.java
+++ eureka-feign/src/main/java/com/eurekafeign/EurekaFeignApplication.java
@@ -2,7 +2,7 @@ package com.eurekafeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 2018/06/25
  */
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaFeignApplication {
 
