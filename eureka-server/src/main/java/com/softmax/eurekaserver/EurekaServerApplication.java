78a0bed0cfd36ff0632d0c0f9a7fcd814626cdaa
jarvisqi <qdw8911@sina.com>
2018-08-23 14:38:05 +0800

    micro service eureka-server

--- eureka-server/src/main/java/com/softmax/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/softmax/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.eurekaserver;
+package com.softmax.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/softmax/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/softmax/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eurekaserver;
+package com.softmax.eurekaserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
