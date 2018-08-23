bf4c2a705704141f37f0052a568724c9a494d4f9
jarvisqi <qdw8911@sina.com>
2018-08-23 22:31:16 +0800

    update

---
+++ config-client/src/main/java/com/softmax/configclient/controller/ConfigController.java
@@ -0,0 +1,77 @@
+package com.softmax.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jarvis
+ * @date 2018/8/23
+ */
+@RestController
+public class ConfigController {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @RequestMapping("/dataconfig")
+    public DataConfig getDataConfig() {
+        DataConfig config = new DataConfig(driverClassName, url, username, password);
+        return config;
+    }
+}
+
+class DataConfig {
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public DataConfig(String driverClassName, String url, String username, String password) {
+        this.driverClassName = driverClassName;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
