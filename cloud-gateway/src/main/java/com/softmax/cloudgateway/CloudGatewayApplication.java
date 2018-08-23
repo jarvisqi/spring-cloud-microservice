5feb6eb698968d3fe3120bd5192bb7efdb909c54
jarvisqi <qdw8911@sina.com>
2018-08-23 14:30:48 +0800

    spring cloud gateway

--- cloud-gateway/src/main/java/com/softmax/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/softmax/cloudgateway/CloudGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.cloudgateway;
+package com.softmax.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
