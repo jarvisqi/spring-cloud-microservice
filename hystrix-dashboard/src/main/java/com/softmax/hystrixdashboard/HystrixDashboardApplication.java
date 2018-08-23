8978909b39969a138bbbb0e95ffd1b6e97da764e
jarvisqi <qdw8911@sina.com>
2018-08-23 14:46:35 +0800

    micro service hystrix dashboard

--- hystrix-dashboard/src/main/java/com/softmax/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/softmax/hystrixdashboard/HystrixDashboardApplication.java
@@ -1,4 +1,4 @@
-package com.hystrixdashboard;
+package com.softmax.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
