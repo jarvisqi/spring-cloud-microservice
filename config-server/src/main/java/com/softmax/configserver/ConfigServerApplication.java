613e450f4c73b5b13e4fcbc4d3ef40d196ed51fa
jarvisqi <qdw8911@sina.com>
2018-08-23 16:27:19 +0800

    micro service config-server

---
+++ config-server/src/main/java/com/softmax/configserver/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.softmax.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心服务端
+ *
+ * @author Jarvis
+ * @date 2018/08/23
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
